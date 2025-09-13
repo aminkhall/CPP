6e6b4b941816aeac205659eec201a4d5af9ccb8c
Mohamed Amine Khallouk <mkhallou@e1r5p7.1337.ma>
2025-09-13 16:02:08 +0100

    cpp03ex00

---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,23 @@
+#ifndef CLAP_TRAP_HPP
+#define CLAP_TRAP_HPP
+
+#include <iostream>
+
+class ClapTrap{
+private:
+    std::string name;
+    int hit;
+    int energy;
+    int damage;
+public:
+    ClapTrap();
+    ClapTrap(const std::string& name);
+    ClapTrap(const ClapTrap& other);
+    ClapTrap& operator=(const ClapTrap& other);
+    ~ClapTrap();
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+};
+
+#endif
