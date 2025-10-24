6d8bb29b04db9104c1094b1e352411c17287f87f
Mohamed Amine Khallouk <mkhallou@e1r6p2.1337.ma>
2025-10-24 15:18:28 +0100

    cpp03

--- cpp03/ex03/ScavTrap.hpp
+++ cpp03/ex03/ScavTrap.hpp
@@ -10,11 +10,11 @@ protected:
     bool gate;
 
 public:
-    ScavTrap();                                 // default con
-    ScavTrap(const std::string &name);          // con init name
-    ScavTrap(const ScavTrap &other);            // copy con
-    ScavTrap &operator=(const ScavTrap &other); // copy assign con
-    ~ScavTrap();                                // dest
+    ScavTrap();                                 
+    ScavTrap(const std::string &name);          
+    ScavTrap(const ScavTrap &other);            
+    ScavTrap &operator=(const ScavTrap &other); 
+    ~ScavTrap();                               
     void attack(const std::string &target);
     void guardGate();
 };
