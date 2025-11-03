909da40ffa0300c6c64180450b41a1747461fa24
Mohamed Amine Khallouk <mkhallou@e2r2p14.1337.ma>
2025-11-03 18:34:44 +0100

    cpp04ex03

---
+++ cpp04/ex03/ICharacter.cpp
@@ -0,0 +1,23 @@
+#include "ICharacter.hpp"
+
+ICharacter::ICharacter()
+{
+    std::cout << "ICharacter default constructor called." << std::endl;
+}
+
+ICharacter::ICharacter(const ICharacter &other)
+{
+    std::cout << "ICharacter Copy constructor called." << std::endl;
+    *this = other;
+}
+
+ICharacter &ICharacter::operator=(const ICharacter &other)
+{
+    std::cout << "ICharacter assignment operator called." << std::endl;
+    return *this;
+}
+
+ICharacter::~ICharacter()
+{
+    std::cout << "ICharacter Destructor called." << std::endl;
+}
---
+++ cpp04/ex03/IMateriaSource.cpp
@@ -0,0 +1,23 @@
+#include "IMateriaSource.hpp"
+
+IMateriaSource::IMateriaSource()
+{
+    std::cout << "IMateriaSource default constructor called." << std::endl;
+}
+
+IMateriaSource::IMateriaSource(const IMateriaSource &other)
+{
+    std::cout << "IMateriaSource Copy constructor called." << std::endl;
+    *this = other;
+}
+
+IMateriaSource &IMateriaSource::operator=(const IMateriaSource &other)
+{
+    std::cout << "IMateriaSource assignment operator called." << std::endl;
+    return *this;
+}
+
+IMateriaSource::~IMateriaSource()
+{
+    std::cout << "IMateriaSource Destructor called." << std::endl;
+}
