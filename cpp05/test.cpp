a4e909db7c1132dd729f5597be05730ba498b453
Mohamed Amine Khallouk <mkhallou@e2r3p4.1337.ma>
2025-11-07 20:39:11 +0100

    cpp05ex01

--- cpp05/test.cpp
+++ cpp05/test.cpp
@@ -3,41 +3,49 @@
 
 
 class Number {
-    private:
-        int x;
-    public:
-        void setX(int x){
-            this->x = x;
-        }
-        int getX();
+private:
+    int x;
+
+public:
+    void setX(int x){
+        this->x = x;
+    }
+
+    int getX(){
+        return x;
+    }
 
     class Low : public std::exception {
-        const char *what() const noexcept{
+        const char *what() const throw(){
             return "Low";
         }
     };
 
     class Hight : public std::exception {
-        const char *what() const noexcept {
+        const char *what() const throw(){
             return "Hight";
         }
     };
 
-    void check(){
-        try{
-            if (x < 10){
-                throw Low();
-            }
-            else    
-                throw Hight();
-        }catch (std::exception &e){
-            std::cout << "Caught exception: " << e.what() << std::endl;
-        }
-    }
 };
 
 int main(){
-    Number x;
-    x.setX(2);
-    x.check();
+
+    Number n;
+
+    n.setX(10);
+    std::cout << "N: " << n.getX() << std::endl;
+
+    try{
+        if (n.getX() < 10)
+           throw Number::Low();
+        else if (n.getX() > 10)
+            throw Number::Hight();
+        else 
+            std::cout << "Valid value" << std::endl;
+    }catch (std::exception &e){
+        std::cout << "Exception: " << e.what() << std::endl;
+    }
+    
+    return 0;
 }
